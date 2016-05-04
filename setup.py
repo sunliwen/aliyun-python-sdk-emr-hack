4dc8079d8d1517945542a08673ce8a44162185e4
Liwen <sunliwen@gmail.com>
2016-05-05 01:09:42 +0800

    init aliyun emr sdk hack

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.rst").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="aliyun-python-sdk-emr-hack",
+    version="0.1.0",
+    description="A pip package",
+    license="MIT",
+    author="Liwen",
+    packages=find_packages(exclude=["tests*"]),
+    install_requires=['aliyun-python-sdk-core-emr-hack'],
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+    ]
+)
---
+++ tests/test.py
@@ -0,0 +1,26 @@
+import json
+
+from os import getenv
+from aliyunsdkcore.client import AcsClient
+from aliyunsdkemr.request.v20160408 import ListClustersRequest
+
+print(getenv('ALIYUN_ACCESS_KEY_ID'))
+print(getenv('ALIYUN_ACCESS_KEY_SECRET'))
+client = AcsClient(ak=getenv('ALIYUN_ACCESS_KEY_ID'),
+                   secret=getenv('ALIYUN_ACCESS_KEY_SECRET'),
+                   region_id='cn-hangzhou')
+
+req = ListClustersRequest.ListClustersRequest()
+req.set_RegionId('cn-hangzhou')
+req.set_StatusList(["CREATING", "RUNNING", "IDLE"])
+req.set_accept_format("JSON")
+
+status, headers, body = client.get_response(req)
+if status == 200:
+    regions = json.loads(body)
+    print(regions)
+    cluster = regions['Clusters']['ClusterInfo'][0]
+    print(cluster['Name'])
+else:
+    print('Unexpected errors: status=%d, error=%s' % (status, body))
+
